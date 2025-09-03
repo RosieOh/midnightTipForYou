60482530c28f095226a253a9e3e9ed3199b2f66a
RosieOh <dhxogns920@gmail.com>
2025-09-03 14:22:46 +0900

    FEAT : 비동기 처리 및 배치 스케줄링 어노테이션 추가

--- src/main/java/com/midnighttipforyou/MidnightTipForyouApplication.java
+++ src/main/java/com/midnighttipforyou/MidnightTipForyouApplication.java
@@ -1,11 +1,15 @@
 package com.midnighttipforyou;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableBatchProcessing
+@EnableAsync
 public class MidnightTipForyouApplication {
 
     public static void main(String[] args) {
